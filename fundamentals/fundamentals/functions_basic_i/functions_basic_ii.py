7cfea4e9cdc8b6d56f6c5b69cf0f6d5dab8bbb04
Dashawn <creswellshawn@gmail.com>
2022-08-01 14:59:51 -0700

    functions basic update

---
+++ fundamentals/fundamentals/functions_basic_i/functions_basic_ii.py
@@ -0,0 +1,12 @@
+#1. Countdown 
+# count = 0
+# t = input("Enter Number")
+# t = int(t)
+# while t >= count:
+#     print((t))
+#     t -= 1
+
+#2. Print and Return
+x, y = input("Enter Two Numbers").split()
+print(x)
+return y
