2de8dfbf6f3eae4ed7e69a283835d777bfa96f22
Dashawn <creswellshawn@gmail.com>
2022-08-01 16:23:12 -0700

    functions basic 2 update

--- fundamentals/fundamentals/functions_basic_i/functions_basic_ii.py
+++ fundamentals/fundamentals/functions_basic_i/functions_basic_ii.py
@@ -7,6 +7,16 @@
 #     t -= 1
 
 #2. Print and Return
-x, y = input("Enter Two Numbers").split()
-print(x)
-return y
+# def print_and_return(list):
+#     print(list[0])
+#     return list[1]
+# print (print_and_return([3,77]))
+
+#3. Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
+# Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
+
+# def first_plus_length(list):
+#     return len(list) + (list[0])
+# print(first_plus_length([1,2,3,4,5]))
+
+#4.
