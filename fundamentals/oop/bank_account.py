3c603115b2b3be10154bd1519f579b0170c99ef2
Dashawn <creswellshawn@gmail.com>
2022-08-02 15:39:29 -0700

    update to bank account

---
+++ fundamentals/oop/bank_account.py
@@ -0,0 +1,45 @@
+
+class BankAccount:
+    # don't forget to add some default values for these parameters!
+
+    accounts = []
+
+    def __init__(self, int_rate = 0.01, balance = 0): 
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.accounts.append(self)
+        # your code here! (remember, instance attributes go here)
+        # don't worry about user info here; we'll involve the User class soon
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+        # your code here
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance  -= amount
+        else:
+            self.balance -= 5
+            print("you dont have that kinda of money bro!")
+        return self
+        
+
+    def display_account_info(self):
+        print("Balance ", self.balance)
+        print("Intrest:", self.int_rate * 100, "%")
+        return self
+    def yield_interest(self):
+        if self.balance <= 0:
+            print("Try again next year")
+        else:
+            self.balance += self.balance * self.int_rate
+            self.display_account_info()
+        return self
+
+    @classmethod
+    def print_all_instnaces(cls):
+
+
+
+
+acct1 = BankAccount(.02, 100).deposit(300).withdraw(200).display_account_info()
+acct2 = BankAccount()
--- fundamentals/oop/some_oop.py
+++ fundamentals/oop/some_oop.py
@@ -1,12 +1,12 @@
-# # declare a class and give it name User
-# class User:		
-#     def __init__(self):
-#         self.first_name = "Ada"
-#         self.last_name = "Lovelace"
-#         self.age = 42
+# # # declare a class and give it name User
+# # class User:		
+# #     def __init__(self):
+# #         self.first_name = "Ada"
+# #         self.last_name = "Lovelace"
+# #         self.age = 42
 
-# # Now that you have a class set up with a constructor 
-# # You can assign new variables to new users in the outer scope!
+# # # Now that you have a class set up with a constructor 
+# # # You can assign new variables to new users in the outer scope!
 # user_ada = User()
 # print(user_ada.first_name) # prints Ada
 
@@ -14,60 +14,101 @@
 # print(user_2.first_name) # also prints Ada
 
 
-# declare a class and give it name Shoe
-# class Shoe:		
-#     def __init__(self):
-#         self.brand = "Vans"
-#         self.type = "Classic Sk8-Hi"
-#         self.price = 69.99
-#         self.in_stock = True
+# # declare a class and give it name Shoe
+# # class Shoe:		
+# #     def __init__(self):
+# #         self.brand = "Vans"
+# #         self.type = "Classic Sk8-Hi"
+# #         self.price = 69.99
+# #         self.in_stock = True
 
-# skater_shoe = Shoe()
-# dress_shoe = Shoe()
-# # Accessing the instance's attributes
-# print(skater_shoe.type) # Classic Sk8-Hi
-# print(dress_shoe.type)	# Classic Sk8-Hi
+# # skater_shoe = Shoe()
+# # dress_shoe = Shoe()
+# # # Accessing the instance's attributes
+# # print(skater_shoe.type) # Classic Sk8-Hi
+# # print(dress_shoe.type)	# Classic Sk8-Hi
 
-# skater_shoe.type = "Low-top Trainers"
-# print(skater_shoe.type)
-# # output: Low-top Trainers
-# dress_shoe.type = "Ballet Flats"
-# print(dress_shoe.type)
-# # output: Ballet Flats
+# # skater_shoe.type = "Low-top Trainers"
+# # print(skater_shoe.type)
+# # # output: Low-top Trainers
+# # dress_shoe.type = "Ballet Flats"
+# # print(dress_shoe.type)
+# # # output: Ballet Flats
 
 
-# class Shoe:
-#     # now our method has 4 parameters (including self)!
-#     def __init__(self, brand, shoe_type, price):
-#     # we assign them accordingly
-#         self.brand = brand
-#         self.type = shoe_type
-#         self.price = price
-#     # the status is set to True by default
-#         self.in_stock = True
-# skater_shoe = Shoe("Vans", "Low-top Trainers", 59.99)
-# dress_shoe = Shoe("Jack & Jill Bootery", "Ballet Flats", 29.99)
-# print(skater_shoe.type)	# output: Low-top Trainers
-# print(dress_shoe.type)	# output: Ballet Flats
+# # class Shoe:
+# #     # now our method has 4 parameters (including self)!
+# #     def __init__(self, brand, shoe_type, price):
+# #     # we assign them accordingly
+# #         self.brand = brand
+# #         self.type = shoe_type
+# #         self.price = price
+# #     # the status is set to True by default
+# #         self.in_stock = True
+# # skater_shoe = Shoe("Vans", "Low-top Trainers", 59.99)
+# # dress_shoe = Shoe("Jack & Jill Bootery", "Ballet Flats", 29.99)
+# # print(skater_shoe.type)	# output: Low-top Trainers
+# # print(dress_shoe.type)	# output: Ballet Flats
 
-# adrien.greeting()
+# # adrien.greeting()
 
-class User:		# here's what we have so far
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-    # adding the greeting method
-    def greeting(self):
-        print(f"Hello, my name is {self.name}")
+# # class User:		# here's what we have so far
+# #     def __init__(self, name, email):
+# #         self.name = name
+# #         self.email = email
+# #     # adding the greeting method
+# #     def greeting(self):
+# #         print(f"Hello, my name is {self.name}")
 
-adrien = User("Adrien", "adion@codingdojo.com")
-cool_person = User("Sadie", "sflick@codingdojo.com")
+# # adrien = User("Adrien", "adion@codingdojo.com")
+# # cool_person = User("Sadie", "sflick@codingdojo.com")
     
-adrien.greeting()
-# prints Hello, my name is Adrien
+# # adrien.greeting()
+# # # prints Hello, my name is Adrien
     
-cool_person.greeting()
-# prints Hello, my name is Sadie
+# # cool_person.greeting()
+# # # prints Hello, my name is Sadie
 
+
+# class BankAccount:
+#     # class attributes
+#     bank_name = "First National Dojo"
+#     # new class attribute - a list of all the accounts!
+#     all_accounts = []
+    
+#     def __init__(self, int_rate,balance):
+#         self.int_rate = int_rate
+#         self.balance = balance
+#         BankAccount.all_accounts.append(self)
+#     # class method to change the name of the bank
+#     @classmethod
+#     def change_bank_name(cls,name):
+#         cls.bank_name = name
+#     # class method to get balance of all accounts
+#     @classmethod
+#     def all_balances(cls):
+#         sum = 0
+#         # we use cls to refer to the class
+#         for account in cls.all_accounts:
+#             sum += account.balance
+#         return sum
+
+#     # ... __init__ goes here
+#     def with_draw(self,amount):
+#         # we can use the static method here to evaluate
+#         # if we can with draw the funds without going negative
+#         if BankAccount.can_withdraw(self.balance,amount):
+#             self.balance -= amount
+#         else:
+#             print("Insufficient Funds")
+#         return self
+#     # static methods have no access to any attribute
+#     # only to what is passed into it
+#     @staticmethod
+#     def can_withdraw(balance,amount):
+#         if (balance - amount) < 0:
+#             return False
+#         else:
+#             return True
 
 
