16e7a02d062d8a380da8112db842d65efa1d282d
Dashawn <creswellshawn@gmail.com>
2022-08-04 16:16:15 -0700

    html practice html

---
+++ flask/html_table_practice/README.md
@@ -0,0 +1,14 @@
+## Flask Routes
+
+```
+mkdir flask_routes
+cd flask_routes
+```
+
+```
+pipenv install flask
+pipenv shell
+
+```
+- [ ] make [server.py](server.py)
+- [ ] make [index.html](templates/index.html)
---
+++ flask/html_table_practice/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    users = [
+        {'first_name': 'Dashawn', 'last_name': 'Creswell'},
+        {'first_name': 'Omar', 'last_name': 'Hizzy'},
+        {'first_name': 'Daddy', 'last_name': 'Gojo'},
+        {'first_name': 'Ben', 'last_name': 'Dee'}
+    ]
+    return render_template("index.html", users = users)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
