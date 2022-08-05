8e1505ccead2b915a7979f00a7dde12ea4193bde
Dashawn <creswellshawn@gmail.com>
2022-08-04 17:52:45 -0700

    html table practice

--- flask/html_table_practice/server.py
+++ flask/html_table_practice/server.py
@@ -7,7 +7,7 @@ def hello_world():
     users = [
         {'first_name': 'Dashawn', 'last_name': 'Creswell'},
         {'first_name': 'Omar', 'last_name': 'Hizzy'},
-        {'first_name': 'Daddy', 'last_name': 'Gojo'},
+        {'first_name': 'Him', 'last_name': 'Gojo'},
         {'first_name': 'Ben', 'last_name': 'Dee'}
     ]
     return render_template("index.html", users = users)
