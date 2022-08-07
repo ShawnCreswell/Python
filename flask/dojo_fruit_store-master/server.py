7959aeda7623cf47ab56d0f4b9774f81c3dcc5f8
Dashawn <creswellshawn@gmail.com>
2022-08-06 20:06:05 -0700

    dojo fruit store update

--- flask/dojo_fruit_store-master/server.py
+++ flask/dojo_fruit_store-master/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)  
+app.secret_key = "money"
 
 @app.route('/')         
 def index():
@@ -8,6 +9,16 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
+    session['strawberry'] = request.form['strawberry']
+    session['raspberry'] = request.form['raspberry']
+    session['apple'] = request.form['apple']
+    session['first_name'] = request.form['first_name']
+    session['last_name'] = request.form['last_name']
+    session['student_id'] = request.form['student_id']
+
+    print("Charging", request.form['first_name'], request.form['last_name'], "for", int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple']), "fruits" )
+
+
     return render_template("checkout.html")
 
 @app.route('/fruits')         
