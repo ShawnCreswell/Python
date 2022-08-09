88d9e414e0c05c2e218623a594eea1c71aa77000
Dashawn <creswellshawn@gmail.com>
2022-08-09 09:01:28 -0700

    dojos and ninjas

---
+++ flask_mysql/dojos_and_ninjas/dojos_and_ninjas.sql
@@ -0,0 +1,13 @@
+-- SELECT first_name as First, last_name as Last FROM users;
+
+-- Select * FROM users
+-- WHERE id = 1 OR id = 2
+-- ORDER BY birthday DESC
+
+-- Select * FROM users
+-- WHERE id = 1 OR id = 2
+-- ORDER BY birthday ASC
+
+
+
+
