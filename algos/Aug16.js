9e6badca5ddfc856ba7709f422cc75e57cd27385
Dashawn <creswellshawn@gmail.com>
2022-08-16 09:59:45 -0700

    rotate alog

---
+++ algos/Aug16.js
@@ -0,0 +1,28 @@
+const str = "Hello World";
+
+const rotateAmnt1 = 0;
+const expected1 = "Hello World";
+
+const rotateAmnt2 = 1;
+const expected2 = "dHello Worl";
+
+const rotateAmnt3 = 2;
+const expected3 = "ldHello Wor";
+
+const rotateAmnt4 = 4;
+const expected4 = "orldHello W";
+
+const rotateAmnt5 = 13;
+const expected5 = "ldHello W";
+
+function rotatestr(str,amt){
+    var newstring =""
+    for (let i=str.length-amt; i<str.length;i++) {
+        newstring +=(str[i])
+    }
+    for (let i=0;i<str.length-amt;i++){
+        newstring += (str[i]);
+    }
+    return newstring
+}
+console.log(rotatestr("Hello World",11))
